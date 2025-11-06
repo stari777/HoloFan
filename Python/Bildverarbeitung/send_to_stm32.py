0b01cc939422ed6603ea830e1815c102f8f51106
stari777 <samad.vitambaev@bulme.at>
2025-11-06 12:37:34 +0100

    Init Projects

---
+++ Python/Bildverarbeitung/send_to_stm32.py
@@ -0,0 +1,46 @@
+import serial
+import time
+
+# -----------------------------
+# Konfiguration
+# -----------------------------
+PORT = "COM4"       # COM-Port, an dem der STM32 hängt
+BAUD = 115200       # Baudrate muss mit STM32 übereinstimmen
+FILE_PATH = "CoordToPolar"  # Vorbereitete Bilddatei
+TIMEOUT = 1         # Sekunden, wie lange readline auf Daten wartet
+MAX_WAIT = 30        # Sekunden, maximal auf READY warten
+# -----------------------------
+
+# Serial initialisieren
+ser = serial.Serial(PORT, BAUD, timeout=TIMEOUT)
+ser.reset_input_buffer()
+
+# Handshakex
+print(f"Warte auf Rückmeldung vom uC an {PORT}...")
+t0 = time.time()
+ready = False
+
+while time.time() - t0 < MAX_WAIT: # time.time vergangene Zeit - t0 Startzeitpunkt
+    line = ser.readline().decode(errors='ignore').strip()  # Zeile vom µC lesen
+    if line:  # nur wenn tatsächlich etwas empfangen wurde
+        print("Nucleo:", line)
+    if line == "READY":
+        ready = True
+        break
+
+if not ready:
+    print("Keine Rückmeldung")
+else:
+    print("Rückmeldung empfangen -> starte Dateiübertragung.")
+    with open(FILE_PATH, "r") as f:
+        for line in f:
+            line = line.strip()
+            ser.write(line.encode() + b'\n')       # Zeile senden
+            time.sleep(0.002)               # kleine Pause, um STM32 nicht zu überlasten
+            print(line)
+            resp = ser.readline().decode(errors='ignore').strip()
+            if resp:
+                print("STM32:", resp)
+
+print("Dateiübertragung abgeschlossen.")
+ser.close()
